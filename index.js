00b6c852238dc828faa814d126f41c9fa14f182c
Prottush Hossain <pzh@uw.edu>
2022-01-13 19:54:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const ip = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.get('/pbp-shotchart', (req, res) => {
   req.query.color1 === 'red'  // true
